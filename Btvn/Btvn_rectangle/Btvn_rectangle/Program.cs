ed8621640c293266172130cf50298ef2c4dd8aa2
DaoNhutThi <114686399+DaoNhutThi@users.noreply.github.com>
2023-07-22 23:13:25 +0700

    add_Btvn_*

---
+++ Btvn/Btvn_rectangle/Btvn_rectangle/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Nhap chieu rong cua hinh chu nhat: ");
+int width = int.Parse(Console.ReadLine());
+
+
+Console.Write("Nhap chieu cao cua hinh chu nhat: ");
+int heigh = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Hinh chu nhat");
+
+for (int i = 0; i < heigh; i++)
+{
+    for (int j = 0; j < width; j++)
+    {
+        Console.Write("*");
+    }
+    Console.WriteLine();
+}
---
+++ Btvn/Btvn_triangle/Btvn_triangle/Program.cs
@@ -0,0 +1,27 @@
+﻿/*Console.Write("Nhap chieu cao cua tam giac: ");
+int heigt = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Hinh tam giac");
+for (int i = 0; i < heigt; i++)
+{
+    for (int j = 0; j < i; j++)
+    {
+        Console.Write("*");
+    }
+    Console.WriteLine("*");
+}*/
+
+/*
+
+Console.Write("Nhap chieu cao cua tam giac: ");
+int a = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Hinh tam giac");
+for (int i = a; i >= 1; i--)
+{
+    for (int j = 1; j <= i; j++)
+    {
+        Console.Write("*");
+    }
+    Console.WriteLine();
+}*/
