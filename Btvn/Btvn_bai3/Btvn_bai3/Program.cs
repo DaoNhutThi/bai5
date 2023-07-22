6c82613eff7547f4a5db19140fffd53af9f99f89
DaoNhutThi <114686399+DaoNhutThi@users.noreply.github.com>
2023-07-22 21:56:30 +0700

    add_nangCapGame

--- Btvn/Btvn_bai3/Btvn_bai3/Program.cs
+++ Btvn/Btvn_bai3/Btvn_bai3/Program.cs
@@ -1,36 +1,18 @@
-﻿using System;
+﻿int numOne = 0;
+int numTwo = 1;
+Console.WriteLine("Nhap so N: ");
+int n = int.Parse(Console.ReadLine());
 
-class Program
+for (int i = 0; i < n; i++)
 {
-    static void Main()
+    if (i == 0)
     {
-        Console.Write("Nhập số phần tử của dãy Fibonacci: ");
-        int count = int.Parse(Console.ReadLine());
-
-        Console.WriteLine("Dãy Fibonacci gồm {0} phần tử:", count);
-        for (int i = 0; i < count; i++)
-        {
-            long fibonacciNumber = Fibonacci(i);
-            Console.Write(fibonacciNumber + " ");
-        }
+        Console.WriteLine(i);
+        i++;
     }
+    int num = numOne + numTwo;
+    Console.WriteLine(num);
 
-    static long Fibonacci(int n)
-    {
-        if (n <= 1)
-            return n;
-
-        long previous = 0;
-        long current = 1;
-        long fibonacciNumber = 0;
-
-        for (int i = 2; i <= n; i++)
-        {
-            fibonacciNumber = previous + current;
-            previous = current;
-            current = fibonacciNumber;
-        }
-
-        return fibonacciNumber;
-    }
+    numOne = numTwo;
+    numTwo = num;
 }
---
+++ Btvn/Btvn_nangCapGame/Btvn_nangCapGame/Program.cs
@@ -0,0 +1,60 @@
+﻿bool Exit = false;
+int time = 0;
+
+int randomNumber = Random.Shared.Next(0, 100);
+int guessNumber = 1;
+
+while (!Exit)
+{
+    Console.WriteLine("TRo choi doan so");
+    Console.WriteLine("1. Play Game");
+    Console.WriteLine("2. View Time");
+    Console.WriteLine("3. Thoat\n");
+
+    Console.Write("Your choice: ");
+    string choice = Console.ReadLine();
+    switch (choice)
+    {
+        case "1":
+            playGame();
+            break;
+        case "2":
+            viewTime();
+            break;
+        default:
+            Console.WriteLine("Exit");
+            return;
+    }
+}
+
+void viewTime()
+{
+    //time++;
+    Console.WriteLine("Time is: " + time + "\n");
+
+}
+
+void playGame()
+{
+    while (randomNumber != guessNumber)
+    {
+        Console.Write("Moi ban doan so: ");
+        guessNumber = int.Parse(Console.ReadLine());
+        if (guessNumber == randomNumber)
+        {
+            Console.WriteLine("Chuc mung ban! Ban da doan dung \n ");
+
+        }
+        else if (guessNumber > randomNumber)
+        {
+            Console.WriteLine("So ban doan thap xuong \n ");
+        }
+        else
+        {
+            Console.WriteLine("So ban doan cao len \n ");
+        }
+        time++;
+
+    }
+}
+Console.WriteLine("Ket qua random la: " + randomNumber);
